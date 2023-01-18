7044bd0adda22c94c0ebc021723444b32782c300
ronakk4 <ronakkjaveri15@gmail.com>
2023-01-18 23:55:04 +0530

    footer

--- hackniche-website/src/App.js
+++ hackniche-website/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Timeline from './components/Timeline';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Timeline/>
+      <Footer />
     </div>
   );
 }
