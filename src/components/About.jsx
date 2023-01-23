c72d53d72175e4d3ed5ac6f09fbaf3a7266c7e5b
Vineetttt <vineetchotaliya30@gmail.com>
2023-01-24 01:56:27 +0530

    about section-updating the content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,7 +12,14 @@ const AboutSection= () => {
         <img id="planet" alt="planet"  src={Moon}/>
         <div class="about-box">
             <h1>About</h1>
-            <p class="about-text">We welcome you to Essence- the biggest, most fun college fest of Central India. Join us in the celebration of youth and culture. A wide range of events spread over three ecstatic days will make this an event to reminisce. Do not miss this opportunity! We hope to see you there!</p>
+            <p class="about-text">
+            Hackniche is a 24-hour hackathon taking place in DJ Sanghvi College of Engineering, Mumbai, on the 17th and 18th of February 2023.
+            Our hackathon brings talented and ambitious students from all over India together to collaborate and create innovative solutions to real-world problems.
+            Students can register in groups of 2-4 people. The hackathon is going to be conducted in 2 rounds. First, all the registered participants will submit their resumes, 
+            which will be screened by a qualified team on our end and shortlisted. The teams that move on to round 2 will be invited to the offline hackathon happening on campus, 
+            where they will have 24 hours to build a prototype for their chosen problem statement.
+            Arrangements will be made for refreshments, resting, and recreational activities for our participants.
+            </p>
         </div>  
     </div>
   )
