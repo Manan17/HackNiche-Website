e8d8acc6e5677beffdc199c227f0e07d0d812fef
Aesha <aeshashah2634@gmail.com>
2023-01-24 15:11:32 +0530

    NavBar details updated

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,10 +11,11 @@ const NavBar = () => {
     <nav id="nav" data-aos="menu-area" data-aos-anchor-placement="bottom-top" >
         <a title="About" href="#about_anchor">About</a>
         <a title="Timeline" href="#timeline_anchor">Timeline</a>
-        <a title="Pronites" href="#pronites_anchor">Pronites</a>
+        <a title="Prizes" href="#prizes_anchor">Prizes</a>
+        <a title="Domain" href="#domain_anchor">Domain</a>
         <a title="Contact" href="#contact_anchor">Contact</a>
         <a title="Sponsors" href="#sponsors_anchor">Sponsors</a>
-        <a title="Profile" href="./profile/" class="profile show"></a>
+        {/* <a title="Profile" href="./profile/" class="profile show"></a> */}
     </nav>
     )
 }
