e293035d7b2834581268c5eeb0548850556d548e
Vineetttt <vineetchotaliya30@gmail.com>
2023-01-31 13:22:04 +0530

    about section changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,18 +16,24 @@ const AboutSection = () => {
       <div class="about-box">
         <h1 className="mb-4">About</h1>
         <p class="about-text">
-          Hackniche is a 24-hour hackathon taking place in DJ Sanghvi College of
-          Engineering, Mumbai, on the 17th and 18th of February 2023. Our
-          hackathon brings talented and ambitious students from all over India
+        <ul>
+            <br/>
+          <li>Hackniche is a 24-hour hackathon taking place in DJ Sanghvi College of
+          Engineering, Mumbai, on the 17th and 18th of February 2023.</li>
+          <li>Our hackathon brings talented and ambitious students from all over India
           together to collaborate and create innovative solutions to real-world
-          problems. Students can register in groups of 2-4 people. The hackathon
-          is going to be conducted in 2 rounds. First, all the registered
-          participants will submit their resumes, which will be screened by a
-          qualified team on our end and shortlisted. The teams that move on to
+          problems.</li>
+          <li>Students can register in groups of 2-4 people. The hackathon
+          is going to be conducted in 2 rounds.</li>
+          <li>First, all the registered participants will submit their resumes, which will be screened by a
+          qualified team on our end and shortlisted. </li>
+          <li>The teams that move on to
           round 2 will be invited to the offline hackathon happening on campus,
           where they will have 24 hours to build a prototype for their chosen
-          problem statement. Arrangements will be made for refreshments,
-          resting, and recreational activities for our participants.
+          problem statement. </li>
+          <li>Arrangements will be made for refreshments,
+          resting, and recreational activities for our participants.</li>
+          </ul>
         </p>
       </div>
     </div>
